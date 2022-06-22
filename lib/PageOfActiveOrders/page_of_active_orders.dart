329b0389291e942373eff8ccaa25c9e8d57010fe
r-kulik <rambudals@mail.ru>
2022-06-22 21:46:45 +0300

    displaying tickets

--- lib/PageOfActiveOrders/page_of_active_orders.dart
+++ lib/PageOfActiveOrders/page_of_active_orders.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import '../PageOfActiveOrders/app_bar.dart';
 import '../navigation_bar.dart';
 import 'set_ticket.dart';
+import 'package:inno_cart/backend_functions.dart';
+import 'package:http/http.dart' as http;
 
 class PageOfActiveOrders extends StatefulWidget {
   const PageOfActiveOrders({Key? key}) : super(key: key);
@@ -36,13 +40,44 @@ class PageOfActiveOrdersState extends State<PageOfActiveOrders> {
 
 class Tickets {
   List<Widget> waitingForAccept = [];
+  BuildContext context;
 
-  Tickets(BuildContext context) {
-    waitingForAccept =
-        Ticket(context) + Ticket(context) + Ticket(context);
+  Tickets(this.context) {
+    print("Tickets class has been initialized!");
   }
 
   Future<List<Widget>> getTickets() async {
+    //waitingForAccept =
+    //    Ticket(context) + Ticket(context) + Ticket(context);
+    print("Running function get Tickets");
+    Map<String, dynamic> args = {
+      "user_id": currentUser.userId,
+      "user_token": currentUser.passwordHash
+    };
+    print("Map of arguments had been constructed correctly");
+    print(serverURL);
+    print(args);
+    Uri uri = Uri.parse(
+        "http://127.0.0.1:8000/getTicketsForUser?user_id=${currentUser.userId}&user_token=${currentUser.token}"
+    );
+    print(uri);
+    http.Response response = await http.get(uri);
+    print(response.statusCode);
+
+    if (response.statusCode == 200){
+      Map<String, dynamic> jsonData = jsonDecode(
+        response.body.replaceAll("'", '"').replaceAll("None", '\"\"')
+      );
+      print(jsonData['tickets'][0]);
+      for (Map<String, dynamic> ticketNote in jsonData['tickets']){
+        print(ticketNote);
+        waitingForAccept.add(Ticket(ticketNote));
+      }
+    }
+    else{
+      return [];
+    }
+    print(waitingForAccept);
     return waitingForAccept;
   }
 }
--- lib/PageOfActiveOrders/set_ticket.dart
+++ lib/PageOfActiveOrders/set_ticket.dart
@@ -5,173 +5,184 @@ import '../elevated_button_style.dart';
 import '../main.dart';
 import 'pop_up_window_with_ticket.dart';
 
-List<Widget> Ticket(BuildContext context) {
-  String buttonText = 'Send request';
-  String profilePicture = 'assets/images/man1.png';
-  String orderImage = 'assets/images/pizza.jpg';
-  String orderName = 'Pizza';
-  String orderWeight = '3 kg';
-  String orderDistance = '124 m';
+
+class Ticket extends StatelessWidget{
+  static const String buttonText = 'Send request';
+  static const String profilePicture = 'assets/images/man1.png';
+  static const String orderImage = 'assets/images/pizza.jpg';
+  String? orderName;
+  double orderWeight = 3.0;
+  static const String orderDistance = '124 m';
   String orderTime = '14:00';
   String orderDate = '03.06.2022';
   String orderPrice = '200';
   String orderInfo = 'I want two DoDo peperoni pizzas, thank you!';
-  String shopperName = 'Amr. T.';
+  String? shopperName;
 
-  return [
-    GestureDetector(
-        onTap: () => popUpTicket(context),
-        child: Container(
-          margin: const EdgeInsets.only(bottom: bottomPadding),
-          padding: const EdgeInsets.all(20),
-          width: 345,
-          height: 230,
-          color: Colors.white,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //PICTURE
-                  Container(
-                    width: 130,
-                    height: 130,
-                    color: Colors.blueGrey,
-                    //margin: const EdgeInsets.only(top: 12, left: 12, bottom: 10),
-                    child: Image.asset(
-                      orderImage,
-                      fit: BoxFit.fill,
+
+  Ticket(Map<String, dynamic> data, {Key? key}) : super(key: key){
+    print('Initialization of ticket entity');
+    this.shopperName = data['user_id'].toString();
+    this.orderName = data['title'];
+  }
+
+  Widget build(BuildContext context) {
+    return
+      GestureDetector(
+          onTap: () => popUpTicket(context),
+          child: Container(
+            margin: const EdgeInsets.only(bottom: bottomPadding),
+            padding: const EdgeInsets.all(20),
+            width: 345,
+            height: 230,
+            color: Colors.white,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //PICTURE
+                    Container(
+                      width: 130,
+                      height: 130,
+                      color: Colors.blueGrey,
+                      //margin: const EdgeInsets.only(top: 12, left: 12, bottom: 10),
+                      child: Image.asset(
+                        orderImage,
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                  ),
 
-                  //TICKET INFO
+                    //TICKET INFO
 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        orderName,
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/Bag_alt_light.svg',
-                            color: Colors.black,
-                            width: 24,
-                            height: 24,
-                          ),
-                          Container(
-                              margin: const EdgeInsets.only(left: 10),
-                              child: Text(orderWeight)),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/Pin_alt_light.svg',
-                            color: Colors.black,
-                            width: 24,
-                            height: 24,
-                          ),
-                          Container(
-                              margin: const EdgeInsets.only(left: 10),
-                              child: Text(orderDistance)),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/Time_light.svg',
-                            color: Colors.black,
-                            width: 24,
-                            height: 24,
-                          ),
-                          Container(
-                              margin: const EdgeInsets.only(left: 10),
-                              child: Text(orderTime)),
-                        ],
-                      ),
-                    ],
-                  ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          orderName!,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Bag_alt_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text(orderWeight.toString() + "KG")),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Pin_alt_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text(orderDistance)),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Time_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text(orderTime)),
+                          ],
+                        ),
+                      ],
+                    ),
 
-                  Container(
-                      color: Colors.yellowAccent,
-                      padding: const EdgeInsets.all(6),
-                      child: Row(
-                        children: [
-                          Text(orderPrice),
-                          SvgPicture.asset('assets/icons/Currency.svg'),
-                        ],
-                      )),
-                ],
-              ),
-              //Button
-              Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            radius: (20),
-                            backgroundImage: AssetImage(profilePicture),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Column(
-                              children: [
-                                Text(shopperName),
-                                RatingBar.builder(
-                                  initialRating: 5,
-                                  ignoreGestures: true,
-                                  direction: Axis.horizontal,
-                                  allowHalfRating: true,
-                                  itemCount: 5,
-                                  itemSize: 10,
-                                  itemBuilder: (context, _) => const Icon(
-                                    Icons.star,
-                                    color: Colors.amber,
+                    Container(
+                        color: Colors.yellowAccent,
+                        padding: const EdgeInsets.all(6),
+                        child: Row(
+                          children: [
+                            Text(orderPrice),
+                            SvgPicture.asset('assets/icons/Currency.svg'),
+                          ],
+                        )),
+                  ],
+                ),
+                //Button
+                Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: (20),
+                              backgroundImage: AssetImage(profilePicture),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15),
+                              child: Column(
+                                children: [
+                                  Text(shopperName!),
+                                  RatingBar.builder(
+                                    initialRating: 5,
+                                    ignoreGestures: true,
+                                    direction: Axis.horizontal,
+                                    allowHalfRating: true,
+                                    itemCount: 5,
+                                    itemSize: 10,
+                                    itemBuilder: (context, _) =>
+                                    const Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                    ),
+                                    onRatingUpdate: (rating) {
+                                      print(rating);
+                                    },
                                   ),
-                                  onRatingUpdate: (rating) {
-                                    print(rating);
-                                  },
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            //popUpRequestSentNotifier(context);
-                          },
-                          style: RoundedWhite,
-                          child: SizedBox(
-                            width: 150,
-                            height: 32,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  buttonText,
-                                  style: const TextStyle(
-                                      fontSize: 14, color: Colors.black),
-                                ),
-                                const Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.black,
-                                  size: 14,
-                                ),
-                              ],
-                            ),
-                          )),
-                    ],
-                  )),
-            ],
-          ),
-        ))
-  ];
+                          ],
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              //popUpRequestSentNotifier(context);
+                            },
+                            style: RoundedWhite,
+                            child: SizedBox(
+                              width: 150,
+                              height: 32,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment
+                                    .spaceBetween,
+                                children: [
+                                  Text(
+                                    buttonText,
+                                    style: const TextStyle(
+                                        fontSize: 14, color: Colors.black),
+                                  ),
+                                  const Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.black,
+                                    size: 14,
+                                  ),
+                                ],
+                              ),
+                            )),
+                      ],
+                    )),
+              ],
+            ),
+          ));
+  }
 }
