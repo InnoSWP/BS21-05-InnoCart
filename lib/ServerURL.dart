976f81fa4c3b8984e6613cff67e3d5f4893ce870
hir0github <almazdautov1234523@gmail.com>
2022-07-02 19:32:25 +0300

    code fixes for future maintainability

--- lib/ServerURL.dart
+++ lib/ServerURL.dart
@@ -1,3 +1,3 @@
-const String serverURL = "10.91.52.107:8000";
+const String serverURL = "192.168.3.36:8000";
 // wifi "192.168.3.36";
 // 10.91.52.107
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       initialRoute: '/',
       onGenerateRoute: generateRoute,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
