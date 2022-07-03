97281e556ed773741fe19cc9508c3338dc4cac27
hir0github <almazdautov1234523@gmail.com>
2022-07-03 22:24:26 +0300

    Pixels overflow fixes

--- lib/UI/Blocks/TicketBlock.dart
+++ lib/UI/Blocks/TicketBlock.dart
@@ -26,8 +26,6 @@ class TicketBlock extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(bottom: bottomPadding),
         padding: const EdgeInsets.all(20),
-        width: 345,
-        height: 245,
         color: Colors.white,
         child: Column(children: [
           Container(
@@ -37,86 +35,92 @@ class TicketBlock extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 //PICTURE
-                Container(
-                  width: 130,
-                  height: 130,
-                  color: Colors.blueGrey,
-                  //margin: const EdgeInsets.only(top: 12, left: 12, bottom: 10),
-                  child: Image.asset(
-                    ticket.ticketImage,
-                    fit: BoxFit.fill,
-                  ),
+                Flexible(
+                  flex: 3,
+                  child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGrvu5dvNWm3aeTwcEfGy5uW2nTSI6dMU-ENCRvcL7UGS7sEYfNTvhFx6_gnajDWE8uLQ&usqp=CAU'),
                 ),
 
                 //TICKET INFO
-
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 170,
-                      child: Text(
-                        ticket.title,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                    ),
-                    Row(
+                Flexible(
+                  flex: 5,
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        SvgPicture.asset(
-                          'assets/icons/Bag_alt_light.svg',
-                          color: Colors.black,
-                          width: 24,
-                          height: 24,
+                        Text(
+                          ticket.title,
+                          overflow: TextOverflow.visible,
+                          style: const TextStyle(fontSize: 18),
                         ),
-                        Container(
-                            margin: const EdgeInsets.only(left: 10),
-                            child: Text("${ticket.weight} KG")),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Bag_alt_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text("${ticket.weight} KG")),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Pin_alt_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text(ticket.distance)),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/Time_light.svg',
+                              color: Colors.black,
+                              width: 24,
+                              height: 24,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(left: 10),
+                                child: Text(ticket.deadlineUnixTime)),
+                          ],
+                        ),
                       ],
                     ),
-                    Row(
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icons/Pin_alt_light.svg',
-                          color: Colors.black,
-                          width: 24,
-                          height: 24,
-                        ),
-                        Container(
-                            margin: const EdgeInsets.only(left: 10),
-                            child: Text(ticket.distance)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icons/Time_light.svg',
-                          color: Colors.black,
-                          width: 24,
-                          height: 24,
-                        ),
-                        Container(
-                            margin: const EdgeInsets.only(left: 10),
-                            child: Text(ticket.deadlineUnixTime)),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-                Container(
-                    color: Colors.yellowAccent,
-                    padding: const EdgeInsets.all(6),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 50,
-                          child: Text(
-                            ticket.reward.toString(),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        SvgPicture.asset('assets/icons/Currency.svg'),
-                      ],
-                    )),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                      color: Colors.yellowAccent,
+                      padding: const EdgeInsets.all(6),
+                      child: Row(
+                        children: [
+                          Flexible(
+                              flex: 3,
+                              child: SizedBox(
+                                width: 50,
+                                child: Text(
+                                  ticket.reward.toString(),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                          Flexible(
+                              flex: 1,
+                              child: SvgPicture.asset(
+                                  'assets/icons/Currency.svg')),
+                        ],
+                      )),
+                ),
               ],
             ),
           ),
